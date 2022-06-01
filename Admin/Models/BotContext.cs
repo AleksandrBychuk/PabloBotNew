b72a15d2b7e12cc59c055cb8816c205450d41b25
Александр Бычук <sbychuk645@gmail.com>
2022-06-02 02:41:23 +0300

    Add project files.

---
+++ Admin/Models/BotContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Admin.Models
+{
+    public class BotContext : DbContext
+    {
+        public BotContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
---
+++ Admin/Models/User.cs
@@ -0,0 +1,22 @@
+﻿namespace Admin.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public string Discriminator { get; set; }
+    }
+}
+
+/*"id": "809490190913372203"
+"username": "Nikolai"
+"avatar": null
+"avatar_decoration": null
+"discriminator": "3239"
+"public_flags": 0
+"flags": 0
+"banner": null
+"banner_color": null
+"accent_color": null
+"locale": "ru"
+"mfa_enabled": false*/
